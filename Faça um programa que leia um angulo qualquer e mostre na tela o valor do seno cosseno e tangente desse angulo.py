e0a0582c5fdaced45788d5c81e79fcca02c78712
Thiago <thiagooliveira1985@gmail.com>
2021-05-14 12:14:34 -0400

    .
    .

---
+++ Faça um programa que leia um angulo qualquer e mostre na tela o valor do seno cosseno e tangente desse angulo.py
@@ -0,0 +1,6 @@
+import math
+ang = int(input('informe o angulo: '))
+cos = math.cos(ang)
+tang = math.tan(ang)
+sen = math.sin(ang)
+print('O angulo é: {}, O valor cosseno é: {:.2f}, O valor Tangente è: {:.2f}, O seno é: {:.2f}'.format(ang, cos, tang, sen))
